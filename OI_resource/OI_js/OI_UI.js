fe330c9b815acc78ed0d451b6c622defa101f62e
LeeInHaeng <lih0420@naver.com>
2018-01-18 23:22:36 +0900

    0.3ver recent
    this is 0.3 version recent

---
+++ OI_resource/OI_js/OI_UI.js
@@ -0,0 +1,63 @@
+  $("#asset").draggable();
+  $("#asset").resizable({
+      aspectRatio : 16/9,
+      minHeight: 400,
+      minWidth: 400,
+      containment : "#content"
+  });
+
+  $("#assetExplore").draggable();
+
+
+// 오른쪽 메뉴 여닫기-----------------------------------------
+ $(function () {
+     function menutoggle() {
+         var options = {};
+         options = {
+             direction: "right"
+         };
+         $("#sideinfo").toggle("slide", options, 500);
+         $("#button").toggleClass("buttonCloseLo", 500);
+     };
+     $("#button").on("click", function () {
+         menutoggle();
+     });
+ });
+
+
+$(function(){
+    function assetAppClose(){
+        $( "#asset" ).hide( "fold", {}, "slow" );
+    }
+      function assetAppToggle() {
+         $("#asset").toggle("fold");
+     };
+    
+     $("#iconLo1").on("click", function () {
+         assetAppToggle();
+     });
+    
+    $("#assetExit").on("click", function () {
+         assetAppClose();
+     });
+})
+
+// 에셋 익스플로러
+
+$(function(){
+    function ExploreAppClose(){
+        $( "#assetExplore" ).hide( "fold", {}, "slow" );
+    }
+      function ExploreAppToggle() {
+         $("#assetExplore").toggle("fold");
+     };
+    
+     $("#iconLo3").on("click", function () {
+         ExploreAppToggle();
+     });
+    
+    $("#explorer_exit").on("click", function () {
+         ExploreAppClose();
+     });
+})
+
